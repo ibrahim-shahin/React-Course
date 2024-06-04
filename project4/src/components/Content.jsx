ad1cfcfdddd80439e91a7b516c6db9f8c3d2bf34
Ibrahim Shahin <shaheenibrahim1234556@gmail.com>
2024-06-04 19:02:39 +0300

    working on project4

--- project4/src/components/Content.jsx
+++ project4/src/components/Content.jsx
@@ -3,41 +3,67 @@ import '../assets/Content.css'
 import { useEffect } from 'react'
 
 const Content = () => {
-    const [url, setUrl] = useState()
+    const [meme, setMeme] = useState({
+        topText: "",
+        bottomText: "",
+        url: "https://i.imgflip.com/1bij.jpg"
+    })
     const [memes, setMemes] = useState()
-    const [meme, setMeme] = useState(0)
+    const [randomNumber, setRandomNumber] = useState(0)
 
     useEffect(() => {
-        const getMemeImage = async () => {
+        const getMemeImage = () => {
             fetch("https://api.imgflip.com/get_memes")
                 .then(res => res.json())
                 .then(data => setMemes(data.data.memes));
-            setUrl(memes[meme].url)
         }
-
         getMemeImage()
-    })
+    }, [])
 
     function getRandomNumber() {
-        setMeme(Math.floor(Math.random() * 100));
-        setUrl(memes[meme].url)
+        setRandomNumber(Math.floor(Math.random() * 100));
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            url: memes[randomNumber].url
+        }))
     }
 
+    function handleChange(event) {
+        console.log(event.target)
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
 
     return (
         <div className='content'>
             <div className="inputs">
                 <div className='input'>
                     <label htmlFor="top-text">Top text</label>
-                    <input type="text" placeholder='shut up' id='top-text' />
+                    <input
+                        value={meme.topText}
+                        name='topText'
+                        onChange={handleChange}
+                        type="text"
+                        placeholder='shut up'
+                        id='top-text'
+                    />
                 </div>
                 <div className='input'>
                     <label htmlFor="bottom-text">Bottom text</label>
-                    <input type="text" placeholder='and take my money' id='bottom-text' />
+                    <input
+                        value={meme.bottomText}
+                        name='bottomText'
+                        onChange={handleChange}
+                        type="text"
+                        placeholder='and take my money'
+                        id='bottom-text'
+                    />
                 </div>
             </div>
             <input className='button' type="button" value="Get a new meme image  🖼" onClick={getRandomNumber} />
-            {memes && <img className='meme' src={url} alt="" />}
+            {memes && <img className='meme' src={meme.url} alt="" />}
         </div>
     )
 }
